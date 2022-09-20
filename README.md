b8ad7c96b80ac77dd25aaf525bf0e38b07b2602e
JeremyF <jfulbright@gmail.com>
2022-09-20 08:40:25 -0600

    Added timeblock styling and finished functionality

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # JavaScript-Day-Scheduler
-Simple calendar application that allows a user to save events for each hour of the day.
+Jeremy Fulbright - Day Scheduler Project
+
+## Project Resources:
+
+- [ ] [GitHub Repo](https://github.com/jfulbright/JavaScript-Day-Scheduler)
+- [ ] [GitHub Pages Demo](https://jfulbright.github.io/Javascript-Day-Scheduler)
+
+
+***
+
+
+
+## Description
+The objective of this project was to create a javascript application for a simple calendar application that allows a user to save events for each hour of the day to localStorage. The timeblocks will have a visual styling depending if they're in the past, present and future. 
+
+
+## Visuals
+### Past Event UI Styling: 
+
+![uiz UI](assets/images/JavaScriptSchedule-Past.png "Past Events")
+
+### Present Event  UI Styling:
+
+![Quiz UI](assets/images/JavaScriptSchedule-Present.png "Present Events")
+
+### Future Event UI Styling:
+
+![uiz UI](assets/images/JavaScriptSchedule-Future.png "Future Events")
+
+
+## Installation
+This is a single page webpage using HTML, CSS and JavaScript. It can be installed on any web server.
+
+## Usage
+This site may not be used or copied.
+
+## Roadmap
+Future Roadmap Iterations will include:
+* Code Simplification, UI/UX Enhancements, Improved Error Handling, and the ability to change days.
+
+
+## License
+See  [LICENSE](LICENSE "LICENSE") for more information.
+
+## Project status
+This deployed site is an MVP iteration where future functionality can be added. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,49 @@
+$(document).ready(function init() {
+  // call functions to initialize app
+  SetTimeBlockStyle()
+  getSavedEvents ()
+})
 
 // Declare variable with current time:
-var time = moment().format("dddd, MMMM Do");
+var currentDate = moment().format("dddd, MMMM Do");
 let currentHour = moment().format("HH");
-$("#currentDay").text(time);
-
-// Create an object to store timeblock attributes
+$("#currentDay").text(currentDate);
 
 
 
+// function to display styles based on past, present and future time blocks
+function SetTimeBlockStyle() {
+  $(".time-div").each(function() {
+    var timeDiv = $(this).attr("id");
+    if (currentHour > timeDiv) {
+      $(this).find( "textarea" ).addClass("past");
+    } else if (currentHour < timeDiv) {
+      $(this).find( "textarea" ).addClass("future");
+    } else if (currentHour == timeDiv) {
+      $(this).find( "textarea" ).addClass("present");
+    }
+    
+  })
+}
 
 
+// function to save event description on save button click
+$( ".saveBtn" ).click(function() {
+  var timeBlockID = $(this).parent().attr("id");
+  var timeBlockDesc = $(this).siblings(".time-block").val();
+  // set local storage 
+  localStorage.setItem(timeBlockID, timeBlockDesc);
+});
 
+
+// display saved schedule events from localStorage
+function getSavedEvents (){
+  $(".time-div").each(function() {
+  var timeDiv = $(this).attr("id");
+  // get event description from local storage using div ID as key
+  var localTimeID = localStorage.getItem(timeDiv);
   
+  $(this).find( "textarea" ).text(localTimeID);
 
- 
- 
-  // Function that saves timeblock text to Localstorage
-
-
-  // Function that retrieves timeblock text from Local Storage and displays in timeblock
-
-
-
-  // Function that switches timeblock css id depending on time of day
-    // Previous: if timeblock time (rounded to whol number) is less than current time
-    // Current: if timeblock time is between current time
-
-
-
-
-
-    //ToDo:
-    // App Flow:
-      // Selectable Function identifies timeBlock Index
-      // Event listener captures keydown events
-        // diplays on timeBlock
-        // Saves in variable
-      // Event listener on Save button:
-        // Updates TimeEvent Block with index and keystroke vars. 
-      // Pass timeBlock Index to 
+  });
+} 
